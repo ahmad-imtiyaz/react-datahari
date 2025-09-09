f83c9c36bed356430906b21da5d6e633c56c4ac4
Ahmad Imtiyaz <imtiyaznajih8@gmail.com>
2025-09-09 11:25:04 +0700

    Delete src directory

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer style={{ marginTop: "20px", fontSize: "14px", color: "gray" }}>
-      <p>© {new Date().getFullYear()} Data Hari App by Yazna 🚀</p>
-    </footer>
-  );
-}
-
-export default Footer;
